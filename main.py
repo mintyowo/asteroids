5000c66c516f635e02578af26781a56c8b8c844c
mintyowo <mint.blossom@simplygreat.education>
2025-07-18 09:32:00 +0000

    can close the game now

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     """
     print(a)
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill((0,0,0))
         pygame.display.flip()
 
